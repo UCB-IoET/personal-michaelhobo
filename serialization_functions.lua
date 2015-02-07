69dc4ee0f8c343e1ca78260dcf0ae55aa6e970a4
Michael Ho <callmemikeh@gmail.com>
2015-02-07 15:49:30 -0800

    updated serialization code, undone.

--- serialization_functions.lua
+++ serialization_functions.lua
@@ -2,36 +2,34 @@
 id = "1"
 
 available = {
-	["buzzer"]		=	{[0]="[01]"},
-	["blue_LED"]	=	{[0]="[01]"},
+	["buzzer"]		=	{"This is a buzzer. Arg0 is the state to switch it to. (0 is off, 1 is on)", {[0]="[01]"}},
+	["blue_LED"]	=	{"This is a blue LED. Arg0 is the state to switch it to (0 is off, 1 is on)", {[0]="[01]"}},
+	["get_buzzer"] = {"Get the buzzer's state. 0 is off, 1 is on.", {}},
 }
-len = function(table)
-	local count = 0
-	for _ in pairs(table) do count = count + 1 end
-	return count
-end
 
 break_func = function(func_string)
 	local count = 0
 	local arguments = {}
+	local funcname = ""
 	for str in string.gmatch(func_string, "[^:]+") do
 		if count == 0 then
-			local funcname = str
+			funcname = str
 		else
 			arguments[count - 1] = str
 		end
 		count = count + 1
 	end
 	count = count - 1
+	print(funcname)
 	return funcname, arguments, count
 end
 
 call_func = function(func_string)
-	local funcname, local arguments, local count = break_func(func_string)
-	if count == len(available[funcname]) then
-		for key in 0, len(available[funcname]) - 1 do
+	local funcname, arguments, count = break_func(func_string)
+	if count == #available[funcname] then
+		for key = 0, #available[funcname] do
 			if arguments[key]:match(available[funcname][key]) then
-				available[funcname](unpack(args))
+				_G[funcname](unpack(arguments))
 				return true
 			else
 				return false
@@ -41,11 +39,15 @@ call_func = function(func_string)
 end
 
 serialize_available = function()
+	serialize_table(available)
+end
+
+serialize_table = function(table)
 	local msg = id .. ":"
-	for func,arg_list in available do
+	for func,arg_list in pairs(table) do
 		msg = msg .. "{" .. func
-		for arg_num in 0,len(available[func])-1 do
-			msg = msg .. ":" .. available[func][arg_num]
+		for arg_num = 0,#table[func] do
+			msg = msg .. ":" .. table[func][arg_num]
 		end
 		msg = msg .. "}"
 	end
@@ -56,7 +58,8 @@ deserialize_available = function(payload)
 	local node_available = {}
 	for func_string in string.gmatch(payload, "{[^}]+}") do
 		local count = 0
-		local funcname, local arguments, local count = break_func(func_string)
+		local funcname, arguments, count = break_func(string.sub(func_string, 2, -2))
 		node_available[funcname] = arguments
 	end
+	return node_available
 end
