3075cd02f065040a8105ce892ef1bf9a4e016960
Michael Ho <callmemikeh@gmail.com>
2015-02-08 22:45:01 -0800

    yes...

---
+++ demo_node2.lua
@@ -0,0 +1,26 @@
+require("discovery")
+require("storm")
+require("cord")
+shield = require("starter")
+
+make_available("set_blue_led", "sets blue LED", {[0]="[01]"})
+make_available("set_green_led", "sets green LED", {[0]="[01]"})
+
+node("blue-green")
+
+set_blue_led = function(state)
+	if state == "1" then
+		shield.LED.on("blue")
+	elseif state == "0" then
+		shield.LED.off("blue")
+	end
+end
+
+set_green_led = function(state)
+	if state == "1" then
+		shield.LED.on("green")
+	elseif state == "0" then
+		shield.LED.off("green")
+	end
+end
+
