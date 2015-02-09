c454ca003b17de409cc4a48ae2ceae0bfddf831f
Aparna Dhinakaran <aparnadhinak@berkeley.edu>
2015-02-08 23:32:48 -0800

    debugged number val

--- discovery.lua
+++ discovery.lua
@@ -42,10 +42,10 @@ node = function(ID)
 				print(payload)
 				table[from] = deserialize_table(payload)
         if table [from] == nil then
-          table[from] = {[value]= deserialize_table(payload), ["destruct"]={storm.os.invokeLater(5*storm.os.SECOND, delFromTable, from)}}
+          table[from] = {[value]= deserialize_table(payload), ["destruct"]= function () storm.os.invokeLater(5*storm.os.SECOND, delFromTable, from) end }
         else 
           storm.os.cancel(table[from]["destruct"])
-          table[from] = {[value]= deserialize_table(payload), ["destruct"]={storm.os.invokeLater(5*storm.os.SECOND, delFromTable, from)}}
+          table[from] = {[value]= deserialize_table(payload), ["destruct"]= function () storm.os.invokeLater(5*storm.os.SECOND, delFromTable, from) end }
 				end
 			end)
 	priv_sock = storm.net.udpsocket(priv_port,
@@ -55,7 +55,7 @@ node = function(ID)
 					all_good = call_func(string.sub(str, 2, -2)) and all_good
 				end
 			end)
-	storm.os.invokePeriodically(storm.os.SECOND, storm.net.sendto(ssock, serialize_available() , "ff02::1", pub_port))
+	storm.os.invokePeriodically(storm.os.SECOND, function() storm.net.sendto(ssock, serialize_available() , "ff02::1", pub_port) end)
 end
 
 function make_available(func_name, documentation, arguments)
